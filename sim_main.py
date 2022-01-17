0624b718b287a6cfd6f2e5d52d734a28e20c94a9
Paulo Dutra <58897843+Paulo-D2000@users.noreply.github.com>
2022-01-16 23:00:27 -0300

    Update sim_main.py

--- sim_main.py
+++ sim_main.py
@@ -13,16 +13,15 @@ km_h = 1.0/3.6  #m/s
 
 g = -9.89     #m^2/s
 
-h0 = 24384    #m
-v00 = 3675.13 #km/h
-m0 = 1360.777 #kg
-ang0 = 5      #deg
+h0 = 500    #m
+v00 = 300 #km/h
+ang0 = 45      #deg
 
-dt = 10.0
+dt = 0.1
 
 v0 = v00*km_h
 
-t = np.arange(0,30e4,dt)
+t = np.arange(0,20,dt)
 
 vel = np.zeros((2,len(t)))
 pos = np.zeros((2,len(t)))
@@ -33,7 +32,7 @@ pos[:,0] = [0,h0]
 
 for i in range(len(t)-1):
     pos[:,i+1] = pos[:,i] + vel[:,i]*dt
-    vel[:,i+1] = vel[:,i] + np.array([0,g/m0])*dt
+    vel[:,i+1] = vel[:,i] + np.array([0,g])*dt
 
     if(pos[1,i]<0):
         print("Impact!\n Time: %f seconds\n Speed: %f m/s"%(t[i], np.sqrt(vel[0,i]**2+vel[1,i]**2)))
@@ -41,7 +40,7 @@ for i in range(len(t)-1):
 
 
 plt.plot(t[pos[1]>0],pos[1,pos[1]>0],label='angle: %d'%(ang0))
-plt.title('SR-71 "Package" EXPRESS Delivery\nV = %dKm/h, H = %dm, M = %dKg\nV = MACH 3, H = 80000ft, M = 3000 pounds'%(v00,h0,m0))
+plt.title('"Package"\nV = %dKm/h, H = %dm, ang = %d deg'%(v00,h0,ang0))
 plt.xlabel('t (s)')
 plt.ylabel('h (m)')
 plt.legend()
